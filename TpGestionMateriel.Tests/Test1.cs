929b2ae9190da569d12babfff750e75c2b700337
Mercier Allan <Allan.mercier@edouardgand.com>
2026-04-29 15:26:45 +0200

    Ajout du premier test unitaire

--- TpGestionMateriel.Tests/Test1.cs
+++ TpGestionMateriel.Tests/Test1.cs
@@ -1,10 +1,22 @@
-﻿namespace TpGestionMateriel.Tests;
-
-[TestClass]
-public sealed class Test1
-{
-    [TestMethod]
-    public void TestMethod1()
-    {
-    }
-}
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting; 
+using TpGestionMateriel.App; 
+ 
+namespace TpGestionMateriel.Tests; 
+ 
+[TestClass] 
+public class GestionMaterielTests 
+{ 
+    [TestMethod] 
+    public void AjouterMateriel_RetourneTrue_QuandMaterielValide() 
+    { 
+        // Arrange 
+        GestionMateriel gestion = new GestionMateriel(); 
+        OrdinateurPortable pc = new OrdinateurPortable("PC-001", "Dell", "Latitude 5420","Bon", 16, true); 
+ 
+        // Act 
+        bool resultat = gestion.AjouterMateriel(pc); 
+ 
+        // Assert 
+        Assert.IsTrue(resultat); 
+    } 
+} 
